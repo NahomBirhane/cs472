7a327f568fdc4cc5a3a7bcd503c5dbec8b433611
NahomBirhane <nahomyatus@gmail.com>
2023-12-01 18:18:51 -0600

    init

---
+++ lab5/JSIntro.js
@@ -0,0 +1,74 @@
+
+//Q_1 Find the Max Number:
+function max(num1, num2){
+
+    if(num1 > num2){
+        return num1;
+    }else{
+        return num2;
+    }
+   // alternative method using for loop
+// let max = -Infinity;
+// let i ;
+// for(i = 0; i < arguments.length; i++){
+//     if(arguments[i] > max){
+//         max = arguments[i];
+//     }
+// }
+// return max;
+
+}
+let theMax = max(10,20);
+console.log(theMax);
+
+// Q_2 function maxOfThree
+function maxOfThree(){
+    let maxOF3 = -Infinity;
+   for(let i = 0; i < arguments.length; i++){
+       if(arguments[i] > maxOF3){
+        maxOF3 = arguments[i];
+       }
+   }
+   return maxOF3;
+}
+let result = maxOfThree(11,-2,3);
+console.log(result);
+
+//Q_3
+function isVowel(char){
+   let vowels = "aeiouAEIOU";
+   return vowels.includes(char); // include works like contains in java and it returns true or false
+}
+let Vowel = isVowel("e");
+console.log(Vowel);
+
+//Q_4  USING IMPERATIVE PROGRAMMING
+// a function sum()
+function sum(arr){
+    let totalSum = 0;
+    for(let i = 0; i < arr.length; i++){
+       totalSum += arr[i];
+    }
+    return totalSum;
+}
+console.log("The sum of 1 + 2 + 3 + 4 = " + sum([1,2,3,4]));
+
+// b function multiply()
+function multiply(arr){
+    let totalProduct = 1;
+    for(let i = 0; i < arr.length; i++){
+        totalProduct *= arr[i];
+    }
+    return totalProduct;
+}
+console.log("The product of 1*2*3*4 = " + multiply([1,2,3,4]));
+
+//Q_5 function that reverse a string in reverse order
+function reverse(str){
+    let s =" ";
+    for(let i = str.length-1; i >= 0; i--){
+        s += str[i];
+    }
+    return s;
+}
+console.log("The reversed string of jag testar =", reverse("jag testar"))
--- practice/lesson5.js
+++ practice/lesson5.js
@@ -1,8 +1,71 @@
 
-console.log("Hello world this is Javascript!!");
-alert("Example of alert");
+// console.log("Hello world this is Javascript!!");
+// alert("Example of alert");
 
-console.log(a);
-var a;
-a = 5+7
-console.log(a);
+// console.log(a);
+// var a;
+// a = 5+7
+// console.log(a);
+
+// let str = "Nahom"
+// str.indexOf("n")
+
+// function sum(x,y){
+//     return x+y;
+// }
+// console.log(sum(2,3));
+// console.log(sum());
+
+// let username = " Nahom";
+// function greet(){
+//     console.log("hi");
+// }
+// console.log("here is the result ", result + username);
+
+const x = function(num){ return num*num;}
+const finalvalue = x(4);
+console.log(finalvalue);
+console.log(x(2));
+console.log(x(9));
+
+function f(x,y){console.log(x+y);};
+f(5,6); // no overloading in javascript
+
+function log(x,y){
+  console.log("2 arguments " + x + ", " + y);
+}
+
+log();
+log(5);
+log(4,5);
+
+function findMax(){
+    let i;
+    let max = -Infinity;
+    for(i = 0; i < arguments.length; i++){
+        if(arguments[i]>max){
+            max = arguments[i];
+        }
+    }
+    console.log(arguments, arguments.length);
+    return max;
+}
+
+let theMax = findMax(1, 123, 500, 115, 66, 88);
+let theMax1 = findMax(3,8,6);
+console.log(theMax);
+console.log(theMax1);
+
+
+function add(x,y,...more){
+let total = x+y;
+if(more>0){
+    for(let i = 0; i< more.length; i++){
+     total += more[i];
+    }
+}
+return total
+}
+const val = add(5,5,5);
+console.log(val);
+
